816fd7d2c8aa42659adba7d0bfa64b85e854bf87
Jeremy <29926331+j3rr7@users.noreply.github.com>
2023-02-14 09:22:42 +0700

    Update

--- main.py
+++ main.py
@@ -38,21 +38,42 @@ def upload_file():
     result = subprocess.call(['sh', './run.sh'])
 
     print(result)
-  
+
+    base_path = "/home/jere/DeepHuman/examples/"
     base_name, extension = os.path.splitext(filename)
     zip_file_name = base_name + '.zip'
-    out_dir = '/static/downloads/' + zip_file_name
+    out_dir = './static/downloads/' + zip_file_name
+    
+    # grab the model file and move it to static/downloads
+    with zipfile.ZipFile(base_path + base_name + "_intermediate_prepare.zip", 'r') as file:
+    # grab all files wih prefix *_smpl_2.obj inside the zip file
+      for member in file.namelist():
+          if member.endswith("_smpl_2.obj"):
+              file.extract(member, path= "./tmp/" + os.getcwd())
+              break
 
-    with zipfile.ZipFile(zip_file_name, 'w') as zip_file:
-      for root, dirs, files in os.walk("/home/jere/DeepHuman/examples"):
+    src_path = "./tmp/"
+    dir_path = "./static/downloads/"
+
+    # get file named *_smpl_2.obj
+    for root, dirs, files in os.walk(src_path):
+        for file in files:
+            if file.endswith("_smpl_2.obj"):
+                file_path = os.path.join(root, file)
+                shutil.move(file_path, dir_path + "model.obj")
+    
+    shutil.rmtree("./tmp")
+
+    # zip all files with prefix base_name
+    with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+      for root, dirs, files in os.walk(base_path):
         for file in files:
           if base_name in file:
             print("Zipping... ",file)
             file_path = os.path.join(root, file)
-            zip_file.write(file_path)
+            zip_file.write(file_path, os.path.relpath(file_path, start=base_path))
 
-    shutil.move(zip_file_name, 'static/downloads')
-    shutil.move(base_name+'__volume_out_out_detailed.obj', 'static/model.obj')
+    shutil.move(zip_file_name, './static/downloads')
 
     # return 'done <a href="' + out_dir + '">Click Here</a> to download zip file <a href="/">Click Here</a> to try a different image'
     return render_template('done.html', outdir=out_dir, model_filename="{filename}__volume_out_out_detailed.obj"), 200
