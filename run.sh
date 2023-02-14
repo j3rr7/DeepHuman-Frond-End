2174d8d44886de4e9adab1ebd61c52fab85f0186
Jeremy <29926331+j3rr7@users.noreply.github.com>
2023-02-14 07:42:36 +0700

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+cd /home/jere/DeepHuman
+filename="$(basename "/home/jere/pyflask/michael.jpg")"
+mv "/home/jere/pyflask/michael.jpg" "/home/jere/DeepHuman/examples/${filename}"
+python2 main_prepare_natural_img.py --file "/home/jere/DeepHuman/examples/${filename}"&& \ 
+python2 main_infer_natural_img.py --file "/home/jere/DeepHuman/examples/${filename}" &&\ 
+cp "${filename}__volume_out_out_detailed.obj" "/home/jere/pyflask/model.obj"
